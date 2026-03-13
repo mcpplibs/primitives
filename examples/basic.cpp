410b50a7d0f8cfe890f8a8ab08262484e9efeb71
FrozenlemonTee <1115306170@qq.com>
2026-03-14 01:44:33 +0800

    docs: Add examples for traits

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
-import mcpplibs.templates;
+import mcpplibs.primitive;
 
 int main() {
-  std::cout << "=== mcpplibs.templates basic example ===\n";
-  mcpplibs::templates::hello_mcpp();
+  std::cout << "=== mcpplibs.primitive traits example ===\n";
+  std::cout << std::boolalpha;
+  std::cout << "int is std_integer: "
+            << mcpplibs::primitive::std_integer<int> << "\n";
+  std::cout << "double is std_floating: "
+            << mcpplibs::primitive::std_floating<double> << "\n";
+  std::cout << "int is underlying_type: "
+            << mcpplibs::primitive::underlying_type<int> << "\n";
   return 0;
 }
