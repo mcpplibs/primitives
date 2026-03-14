cd90ec7fac0701db979816d18a1ef9641c62fb27
FrozenlemonTee <1115306170@qq.com>
2026-03-14 11:03:24 +0800

    docs: Add examples

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -3,13 +3,24 @@
 import mcpplibs.primitive;
 
 int main() {
-  std::cout << "=== mcpplibs.primitive traits example ===\n";
+  using namespace mcpplibs::primitive;
+  using namespace mcpplibs::primitive::policy;
+
+  std::cout << "=== mcpplibs.primitive traits & policy example ===\n";
   std::cout << std::boolalpha;
-  std::cout << "int is std_integer: "
-            << mcpplibs::primitive::std_integer<int> << "\n";
-  std::cout << "double is std_floating: "
-            << mcpplibs::primitive::std_floating<double> << "\n";
-  std::cout << "int is underlying_type: "
-            << mcpplibs::primitive::underlying_type<int> << "\n";
+  std::cout << "int is std_integer: " << std_integer<int> << "\n";
+  std::cout << "double is std_floating: " << std_floating<double> << "\n";
+  std::cout << "int is underlying_type: " << underlying_type<int> << "\n";
+
+  std::cout << "default value policy is unchecked_value: "
+            << std::is_same_v<policy::default_policies::value,
+                              policy::unchecked_value> << "\n";
+
+  std::cout << "checked_value is a policy_type: "
+            << policy_type<checked_value> << "\n";
+  std::cout << "checked_value category == value: "
+            << (policy::traits<checked_value>::kind == policy::category::value)
+            << "\n";
+
   return 0;
 }
