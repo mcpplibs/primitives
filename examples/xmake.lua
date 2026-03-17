366fc71197d342cd44cb522278101cc871be3543
FrozenlemonTee <frozenlemontee@gmail.com>
2026-03-17 23:50:26 +0800

    Refactor examples to use a loop for target creation

--- examples/xmake.lua
+++ examples/xmake.lua
@@ -2,8 +2,21 @@ add_rules("mode.debug", "mode.release")
 
 set_languages("c++23")
 
-target("basic")
-    set_kind("binary")
-    add_files("basic.cpp")
-    add_deps("mcpplibs-primitives")
-    set_policy("build.c++.modules", true)
+local examples = {
+    "ex01_default_arithmetic",
+    "ex02_type_policy",
+    "ex03_value_policy",
+    "ex04_error_policy",
+    "ex05_concurrency_policy",
+    "ex06_custom_underlying",
+    "ex07_custom_policy",
+    "ex08_custom_operation"
+}
+
+for _, name in ipairs(examples) do
+    target(name)
+        set_kind("binary")
+        add_files(name .. ".cpp")
+        add_deps("mcpplibs-primitives")
+        set_policy("build.c++.modules", true)
+end
